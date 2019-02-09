f1a21e7e7ed6aba072425f8120fd422b04e0371b
Jinah Adam <jinahadam@gmail.com>
2019-02-09 11:14:46 +0800

    Initial Commit

---
+++ CodeWars/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CodeWars
+//
+//  Created by Jinah Adam on 9/2/19.
+//  Copyright © 2019 Jinah Adam. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
