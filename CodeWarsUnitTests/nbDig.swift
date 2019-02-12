c2fd08ad7565ede1682bcbb51d3d0bdd6a7c63ff
Jinah Adam <jinahadam@gmail.com>
2019-02-12 14:29:32 +0800

    nbDig

--- CodeWarsUnitTests/nbDig.swift
+++ CodeWarsUnitTests/nbDig.swift
@@ -10,24 +10,17 @@ import XCTest
 
 class nbDig: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func nbDig(_ n: Int, _ d: Int) -> Int {
+        let kk = (0...n).map { $0 * $0 }
+        return kk.flatMap { Array(String($0)) }.filter { $0 == Character(String(d)) }.count
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        XCTAssertEqual(nbDig(5750, 0), 4700)
+        XCTAssertEqual(nbDig(11011, 2), 9481)
+        XCTAssertEqual(nbDig(12224, 8), 7733)
+        XCTAssertEqual(nbDig(11549, 1), 11905)
     }
 
 }
--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,20 +1,4 @@
 import Cocoa
 
-
-var b = [1,2,3,4]
-
-//let count = a.count - 1
-//for i in 0..<a.count - 1 {
-//    if i == a.count/2 { break }
-//    print(i, count - i)
-//}
-
-let a = [1,2,3,4]
-let a1 = zip(a[0..<a.count/2], a[a.count/2..<a.count].reversed())
-var folded = a1.map { $0 + $1 }
-print(folded)
-if a.count % 2 != 0 {
-    let extra = a[a.count/2]
-    folded.append(extra)
-    print(folded)
-}
+let a = [161, 162, 163, 163, 164, 165, 168, 169, 170, 171]
+print(a.first(where: { ( $0 <= 163 || $0 == 163 ) }))
