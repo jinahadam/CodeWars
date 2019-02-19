f22a6692aaf5846f6f4c98e1bb56f1198031a6ec
Jinah Adam <jinahadam@gmail.com>
2019-02-19 20:53:27 +0800

    SumFractions & Irreducable rational number

---
+++ CodeWarsUnitTests/SumFractions.swift
@@ -0,0 +1,43 @@
+//
+//  SumFractions.swift
+//  CodeWarsUnitTests
+//
+//  Created by Jinah Adam on 19/2/19.
+//  Copyright © 2019 Jinah Adam. All rights reserved.
+//
+
+import XCTest
+
+class SumFractions: XCTestCase {
+
+
+    func rationalAddition(_ a: (a: Int, b: Int),_ b: (c: Int, d: Int)) -> (Int, Int) {
+        return ((a.a * b.d) + (a.b * b.c), a.b * b.d)
+    }
+
+    func gcd(_ frac: (Int, Int)) -> Int {
+        return frac.1 == 0 ? frac.0 : gcd((frac.1, frac.0 % frac.1))
+    }
+
+    func sumFracts(_ l: [(Int, Int)]) -> (Int, Int)? {
+       if l.count == 0 { return nil }
+       let rational = l.dropFirst().reduce(l[0]) { rationalAddition($0, $1) }
+       let factor = gcd(rational)
+       return (rational.0/factor, rational.1/factor)
+    }
+
+    func testing(_ l: [(Int, Int)], _ expected: (Int, Int)?) {
+        XCTAssert(sumFracts(l)! == expected!, "should return \(expected!)")
+    }
+    func testingNil(_ l: [(Int, Int)]) {
+        XCTAssertTrue(sumFracts(l) == nil, "Should return nil")
+    }
+
+    func testExample() {
+        testing([(1, 2), (1, 3), (1, 4)], (13, 12))
+        testing([(1, 3), (5, 3)], (2, 1))
+        testingNil([])
+        testing([(1, 8), (1, 9)], (17, 72))
+    }
+
+}
--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,24 @@
 import Foundation
 
-sqrt()
+func rationalAddition(_ a: (a: Int, b: Int),_ b: (c: Int, d: Int)) -> (Int, Int) {
+    return ((a.a * b.d) + (a.b * b.c), a.b * b.d)
+}
+
+func irreducibleForm(_ frac: (Int, Int)) -> (Int, Int) {
+    for i in (1...min(frac.0, frac.1)/2).reversed() {
+        if frac.0 % i == 0 && frac.1 % i == 0 {
+            return (frac.0/i, frac.1/i)
+        }
+    }
+    return frac
+}
+
+let a = [(1, 2), (1, 3), (1, 4)]// , (13, 12))
+
+//rationalAddition(rationalAddition(a[0], b: a[1]), b: a[2])
+
+
+let sum = a.dropFirst().reduce(a[0]) { rationalAddition($0, $1) }
+print(irreducibleForm(sum))
+
+
