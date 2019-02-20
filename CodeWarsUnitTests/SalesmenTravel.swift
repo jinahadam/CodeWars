7621cb1445bc4e9725c736e5fcffb6eb0f4bca13
Jinah Adam <jinahadam@gmail.com>
2019-02-20 15:27:42 +0800

    Rainfall

---
+++ CodeWarsUnitTests/SalesmenTravel.swift
@@ -0,0 +1,15 @@
+//
+//  SalesmenTravel.swift
+//  CodeWarsUnitTests
+//
+//  Created by Jinah Adam on 20/2/19.
+//  Copyright © 2019 Jinah Adam. All rights reserved.
+//
+
+import XCTest
+
+class SalesmenTravel: XCTestCase {
+
+    
+
+}
