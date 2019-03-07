8e563048e977389f204834fe4b529d25a44f34e3
Jinah Adam <jinahadam@gmail.com>
2019-03-07 13:51:40 +1100

    HiddenCubes

--- CodeWarsUnitTests/HiddenCubes.swift
+++ CodeWarsUnitTests/HiddenCubes.swift
@@ -11,25 +11,15 @@ import XCTest
 class HiddenCubes: XCTestCase {
 
     func isSumOfCubes(_ s: String) -> String {
-        let patter = "(\\d+)"
-        var numbers = [String]()
-        do {
-            let regex = try NSRegularExpression(pattern: patter, options: NSRegularExpression.Options.caseInsensitive)
-            let matches = regex.matches(in: s, options: [], range: NSRange(location: 0, length: s.utf16.count))
-
-            for match in matches {
-                let name = NSString(string: s).substring(with: match.range) as String
-                let prefixIndex = name.index(name.startIndex, offsetBy: name.utf16.count <= 3 ? name.utf16.count : 3)
-                let prefix = String(name.prefix(upTo: prefixIndex)) // Hello
-                numbers.append(prefix)
-            }
-        } catch {}
-
+        let patter = "(\\d{1,3})"
+        let regex = try! NSRegularExpression(pattern: patter, options: NSRegularExpression.Options.caseInsensitive)
+        let matches = regex.matches(in: s, options: [], range: NSRange(location: 0, length: s.utf16.count))
+        let numbers = matches.map {  NSString(string: s).substring(with: $0.range) as String }
         let cubes = numbers.map { $0.characters.map { Int(String($0))! * Int(String($0))! * Int(String($0))! }.reduce(0,+) }
 
         var results = [Int]()
         for i in 0..<cubes.count {
-            if String(cubes[i]) == numbers[i] {
+            if cubes[i] == Int(numbers[i])! {
                 results.append(cubes[i])
             }
         }
@@ -47,6 +37,8 @@ class HiddenCubes: XCTestCase {
         testing("Once upon a midnight dreary, while100 I pondered, 9026315weak and weary -827&()", "Unlucky")
         testing("&z _upon 407298a --- ???ry, ww/100 I thought, 631str*ng and w===y -721&()", "407 407 Lucky")
         testing("&z371 upon 407298a --- dreary, ###100.153 I thought, 9926315strong and weary -127&() 1", "371 407 153 1 932 Lucky")
+        testing("Once 1000upon a midnight 110dreary, while100 I pondered, 9026315weak and weary -827&()", "0 0 Lucky")
+        testing("153000153407000407", "153 0 153 407 0 407 1120 Lucky")
     }
 
 
--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,31 +1,4 @@
 import Foundation
 
-let s = "&z371 upon 407298a --- dreary, ###100.153 I thought, 9926315strong and weary -127&() 1"
-s.replacingOccurrences(of: "(\\d{3})", with: "", options: [.regularExpression, .caseInsensitive], range: nil)
-
-let patter = "(\\d+)"
-var numbers = [String]()
-do {
-    let regex = try NSRegularExpression(pattern: patter, options: NSRegularExpression.Options.caseInsensitive)
-    let matches = regex.matches(in: s, options: [], range: NSRange(location: 0, length: s.utf16.count))
-
-    for match in matches {
-        let range = match.range(at:1)
-        if let swiftRange = Range(range, in: s) {
-            let name = s[swiftRange]
-            print(name.prefix(3))
-            numbers.append(String(name.prefix(3)))
-        }
-    }
-} catch {}
-
-let cubes = numbers.map { $0.characters.map { Int(String($0))! * Int(String($0))! * Int(String($0))! }.reduce(0,+) }
-
-var results = [Int]()
-for i in 0..<cubes.count {
-    if String(cubes[i]) == numbers[i] {
-        results.append(cubes[i])
-    }
-}
-print(results)
-
+"153 153 407 407 1120 Lucky"
+"153 0 153 407 0 407 1120 Lucky"
